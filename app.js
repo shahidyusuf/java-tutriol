f45414e7fcbb22ac348959bfca0b483700b090f9
Muhammad Shahid Yusuf <shd.92yus@gmail.com>
2023-03-11 02:29:27 +0500

    update.home

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-var number1 = 34;
-var number2 = 56;
+// var number1 = 34;
+// var number2 = 56;
 
-console.log(number1+number2);
+// console.log(number1+number2);
+
+//data types in JavaScript..
+// 1.Numbers
+ var num1 = 455;
+ var num2 = 56.76;
+  console.log(num1);
+  console.log(num2)
+
+// 2.String
+ var str1 = "This is a strings";
+ var str2 = "This is a also a strings";
+  console.log(str1);
+  console.log(str2)
+
+// 3.Object
+ var marks = {
+    shani : 34,
+    subhan: 78,
+    jamil : 99.97
+ }
+ console.log(marks)
+
+// 4.Booleans
+ var a = true;
+ var b = false;
+  console.log(a);
+  console.log(b)
+
+// 5.Undefined
+// 6.Null
+
+// At a very high level, there are two types of data type in JavaScript.
+//1. Premative Data Types: Undefined, Null, number, string, boolean, symbol..
+//2. Reference Data Types: Arrays and Objects..
+
+// Array
+ var arr = [1,2,3,4,5];
+ console.log(arr[0])
+ console.log(arr[1])
+ console.log(arr[2])
+ console.log(arr[3])
+ console.log(arr[4])
+
+ var arr = [1,2,"Shahid",4,5];
+ console.log(arr[2])
