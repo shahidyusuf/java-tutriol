14d08579558929b61aade44ebc5f082d820b76c2
Muhammad Shahid Yusuf <shd.92yus@gmail.com>
2023-03-11 13:11:16 +0500

    update.home

--- app.js
+++ app.js
@@ -5,30 +5,30 @@
 
 //data types in JavaScript..
 // 1.Numbers
- var num1 = 455;
- var num2 = 56.76;
-  console.log(num1);
-  console.log(num2)
+//  var num1 = 455;
+//  var num2 = 56.76;
+//   console.log(num1);
+//   console.log(num2)
 
 // 2.String
- var str1 = "This is a strings";
- var str2 = "This is a also a strings";
-  console.log(str1);
-  console.log(str2)
+//  var str1 = "This is a strings";
+//  var str2 = "This is a also a strings";
+//   console.log(str1);
+//   console.log(str2)
 
 // 3.Object
- var marks = {
-    shani : 34,
-    subhan: 78,
-    jamil : 99.97
- }
- console.log(marks)
+//  var marks = {
+//     shani : 34,
+//     subhan: 78,
+//     jamil : 99.97
+//  }
+//  console.log(marks)
 
 // 4.Booleans
- var a = true;
- var b = false;
-  console.log(a);
-  console.log(b)
+//  var a = true;
+//  var b = false;
+//   console.log(a);
+//   console.log(b)
 
 // 5.Undefined
 // 6.Null
@@ -38,12 +38,52 @@
 //2. Reference Data Types: Arrays and Objects..
 
 // Array
- var arr = [1,2,3,4,5];
- console.log(arr[0])
- console.log(arr[1])
- console.log(arr[2])
- console.log(arr[3])
- console.log(arr[4])
+//  var arr = [1,2,3,4,5];
+//  console.log(arr[0])
+//  console.log(arr[1])
+//  console.log(arr[2])
+//  console.log(arr[3])
+//  console.log(arr[4])
 
- var arr = [1,2,"Shahid",4,5];
- console.log(arr[2])
+//  var arr = [1,2,"Shahid",4,5];
+//  console.log(arr[2])
+
+// Arthmetic operators
+// var a = 100;
+// var b = 20;
+
+// console.log("The value of a+b is", a+b);//110
+// console.log("The value of a-b is", a-b);//90
+// console.log("The value of a*b is", a*b);//1000
+// console.log("The value of a/b is", a/b);//10
+
+// Assignment Operators
+// var c = b;
+// c = b;
+// console.log(c);//10
+// var b=10;
+// var c = b;
+// // c+=10;//12
+// // console.log(c);
+// // c-=4;
+// // console.log(c);
+// c*=2; //12
+// console.log(c);
+
+// Comparrison Operators
+
+// var x = 100;
+// var y = 50;
+// console.log(x >= y);//true
+// console.log(x <= y);//false
+// console.log(x == y);//false
+// console.log(x > y);//true
+// console.log(x < y);//false
+
+function avg (a,b){
+    c=(a+b)/2;
+    return c;
+}
+c1 = avg (4,6); //5
+c2 = avg (14,16); //15
+console.log(c1,c2);
